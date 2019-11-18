5132ffa81262132dc770cbbb548031e6c460aa5d
Daredark <44972644+Daredark@users.noreply.github.com>
2019-11-18 21:33:39 +1000

    commit_2

---
+++ colonter.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def logger_alpha_x(s, t):
+    print(s + ' ' + '[' + t + ']')
+
+
+T_START = datetime(2010, 1, 1, 00, 00, 00)
+MAX_USERS = 500000
+MAX_CATEGORIES = 5000
+MAX_MESSAGES = 10000000
+HALF_MESSAGES = 5000000
--- pg_data_generator.py
+++ pg_data_generator.py
@@ -2,8 +2,9 @@ import psycopg2
 import uuid
 import random
 import time
-from datetime import datetime, timedelta
+from datetime import timedelta
 from psycopg2.extras import execute_values
+import colonter
 
 start_time = time.time()
 
@@ -29,97 +30,95 @@ def get_checkpoint_time(x):
 
 time_checkpoint = time.time()
 
-# Создание констант количества данных
-max_users = 500000
-max_categories = 5000
-max_messages = 10000000
-half_messages = 5000000
-
-print('[01/16] | Constants creation completed in...              | ' + get_checkpoint_time(time_checkpoint))
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-time_checkpoint = time.time()
-
 # Создание списков uuid
 author_uuid = []
 category_uuid = []
 message_uuid = []
 
-print('[02/16] | Uuid lists creation completed in...             | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Uuid lists creation completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Заполнение списков uuid
-for i in range(max_users):
+for i in range(colonter.MAX_USERS):
     author_uuid.append(str(uuid.uuid4()))
-for i in range(max_categories):
+for i in range(colonter.MAX_CATEGORIES):
     category_uuid.append(str(uuid.uuid4()))
-for i in range(max_messages):
+for i in range(colonter.MAX_MESSAGES):
     message_uuid.append(str(uuid.uuid4()))
 
-print('[03/16] | Uuid lists filling completed in...              | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Uuid lists filling completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Подготовка переменных для случайного времени отправки сообщения
-t_start = datetime(2010, 1, 1, 00, 00, 00)
-t_end = t_start + timedelta(days=365 * 10)
+t_end = colonter.T_START + timedelta(days=365 * 10)
 
-print('[04/16] | Datetime preparations completed in...           | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Datetime preparations completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Создание списков данных
-users_array = [[0] * 2 for i in range(max_users)]
-categories_array = [[0] * 3 for i in range(max_categories)]
-messages_array = [[0] * 5 for i in range(half_messages)]
-messages_array2 = [[0] * 5 for i in range(half_messages)]
+users_array = [0] * colonter.MAX_USERS
+for i in range(colonter.MAX_USERS):
+    users_array[i] = [0] * 2
 
-print('[05/16] | Data lists creation completed in...             | ' + get_checkpoint_time(time_checkpoint))
+categories_array = [0] * colonter.MAX_CATEGORIES
+for i in range(colonter.MAX_CATEGORIES):
+    categories_array[i] = [0] * 3
+
+messages_array = [0] * colonter.HALF_MESSAGES
+for i in range(colonter.HALF_MESSAGES):
+    messages_array[i] = [0] * 5
+
+messages_array2 = [0] * colonter.HALF_MESSAGES
+for i in range(colonter.HALF_MESSAGES):
+    messages_array2[i] = [0] * 5
+
+colonter.logger_alpha_x('Data lists creation completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Заполнение списка пользователей
-for i in range(max_users):
+for i in range(colonter.MAX_USERS):
     users_array[i][0] = author_uuid[i]
     users_array[i][1] = 'User_' + str(i + 1)
 
-print('[06/16] | List "users_array" filling completed in...      | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('List "users_array" filling completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Заполнение списка категорий
-for i in range(max_categories):
+for i in range(colonter.MAX_CATEGORIES):
     categories_array[i][0] = category_uuid[i]
     categories_array[i][1] = 'Category_' + str(i + 1)
     categories_array[i][2] = category_uuid[i]
 
-print('[07/16] | List "categories_array" filling completed in... | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('List "categories_array" filling completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Заполнение первого списка сообщений
-for i in range(half_messages):
+for i in range(colonter.HALF_MESSAGES):
     messages_array[i][0] = message_uuid[i]
     messages_array[i][1] = 'Text_' + str(i + 1)
     messages_array[i][2] = random.choice(category_uuid)
-    messages_array[i][3] = t_start + (t_end - t_start) * random.random()
+    messages_array[i][3] = colonter.T_START + (t_end - colonter.T_START) * random.random()
     messages_array[i][4] = random.choice(author_uuid)
 
-print('[08/16] | List "messages_array" filling completed in...   | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('List "messages_array" filling completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -128,12 +127,12 @@ time_checkpoint = time.time()
 # Подключение к базе данных
 con = psycopg2.connect(
     host="localhost",
-    database="название_базы_данных",
+    database="название",
     user="postgres",
     password="пароль")
 cur = con.cursor()
 
-print('[09/16] | Database connection completed in...             | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Database connection completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -142,7 +141,7 @@ time_checkpoint = time.time()
 # Вставка пользователей в базу данных
 execute_values(cur, "INSERT INTO users (id, name) VALUES %s", users_array)
 
-print('[10/16] | Users insertion completed in...                 | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Users insertion completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -151,7 +150,7 @@ time_checkpoint = time.time()
 # Вставка категорий в базу данных
 execute_values(cur, "INSERT INTO categories (id, name, parent_id) VALUES %s", categories_array)
 
-print('[11/16] | Categories insertion completed in...            | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Categories insertion completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -160,7 +159,7 @@ time_checkpoint = time.time()
 # Вставка сообщений в базу данных (Часть 1)
 execute_values(cur, "INSERT INTO messages (id, text, category_id, posted_at, author_id) VALUES %s", messages_array)
 
-print('[12/16] | First half messages insertion completed in...   | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('First half messages insertion completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -169,21 +168,21 @@ time_checkpoint = time.time()
 # Удаление первого списка сообщений
 del (messages_array)
 
-print('[13/16] | List "messages_array" deleting completed in...  | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('List "messages_array" deleting completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 time_checkpoint = time.time()
 
 # Заполнение второго списка сообщений
-for i in range(half_messages):
-    messages_array2[i][0] = message_uuid[i + half_messages]
-    messages_array2[i][1] = 'Text_' + str(i + half_messages + 1)
+for i in range(colonter.HALF_MESSAGES):
+    messages_array2[i][0] = message_uuid[i + colonter.HALF_MESSAGES]
+    messages_array2[i][1] = 'Text_' + str(i + colonter.HALF_MESSAGES + 1)
     messages_array2[i][2] = random.choice(category_uuid)
-    messages_array2[i][3] = t_start + (t_end - t_start) * random.random()
+    messages_array2[i][3] = colonter.T_START + (t_end - colonter.T_START) * random.random()
     messages_array2[i][4] = random.choice(author_uuid)
 
-print('[14/16] | List "messages_array2" filling completed in...  | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('List "messages_array2" filling completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -192,7 +191,7 @@ time_checkpoint = time.time()
 # Вставка сообщений в базу данных (Часть 2)
 execute_values(cur, "INSERT INTO messages (id, text, category_id, posted_at, author_id) VALUES %s", messages_array2)
 
-print('[15/16] | Second half messages insertion completed in...  | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Second half messages insertion completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -203,7 +202,7 @@ con.commit()
 cur.close()
 con.close()
 
-print('[16/16] | Closing completed in...                         | ' + get_checkpoint_time(time_checkpoint))
+colonter.logger_alpha_x('Closing completed in', get_checkpoint_time(time_checkpoint))
 
 # ----------------------------------------------------------------------------------------------------------------------
 
